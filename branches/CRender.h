156feced06f254e58203cd68b952aafaa0e5511c
PetrGeorgievsky@gmail.com <PetrGeorgievsky@gmail.com@b33bfa40-0456-428f-48be-daa9cfdfdb8a>
2013-08-07 07:59:30 +0000

    ...

---
+++ branches/CRender.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <d3dx9.h>
+
+class CRender
+{
+	static void BeforeReset();
+	static void AfterReset();
+	static void DefineReset();
+	static void Setup();
+	static D3DXMATRIX *__cdecl _getComposedMatrix(D3DXMATRIX *m_out);
+public:
+	static void Patch();
+	static D3DXMATRIX  m_mTransform;
+};
