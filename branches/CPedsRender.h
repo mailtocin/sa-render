8a6475a26ea307c931df9c31891baaa67d552317
PetrGeorgievsky@gmail.com <PetrGeorgievsky@gmail.com@b33bfa40-0456-428f-48be-daa9cfdfdb8a>
2013-08-07 07:59:15 +0000

    ..

---
+++ branches/CPedsRender.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "RenderWare.h"
+#include <d3dx9.h>
+
+class CPedsRender
+{
+	static HRESULT rxD3D9VertexShaderDefaultMeshRenderCallBack(RxD3D9ResEntryHeader *resEntry, RxD3D9InstanceData *instData);
+public:
+	static void Patch();
+	static bool Setup();
+	static void Reset();
+	static void Lost();
+	static ID3DXEffect *m_pEffect;
+	static D3DXMATRIX m_LightViewProj;
+};
