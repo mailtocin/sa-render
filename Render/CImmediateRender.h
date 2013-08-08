861aea5995f35692e3b09ce24d059d1c5cd22104
dk22_@mail.ru <dk22_@mail.ru@b33bfa40-0456-428f-48be-daa9cfdfdb8a>
2013-08-08 21:10:09 +0000

    Im3d module

---
+++ Render/CImmediateRender.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <d3d9.h>
+
+enum eImmediateRendering
+{
+	IM_RENDER_NOT_DEFINED = 0,
+	IM_RENDER_WATER = 1
+};
+
+class CImmediateRender
+{
+public:
+	static eImmediateRendering m_nCurrentRendering;
+	static void Patch();
+	static HRESULT __fastcall DrawPrimitive_1(int, int, IDirect3DDevice9 *, D3DPRIMITIVETYPE, UINT, UINT);
+	static HRESULT __fastcall DrawPrimitive_2(int, int, IDirect3DDevice9 *, D3DPRIMITIVETYPE, UINT, UINT);
+	static HRESULT __fastcall DrawPrimitiveUp(int, int, IDirect3DDevice9 *, D3DPRIMITIVETYPE, UINT, const void *, UINT);
+	static HRESULT __fastcall DrawIndexedPrimitive(int, int, IDirect3DDevice9 *, D3DPRIMITIVETYPE, INT, UINT, UINT, UINT, UINT);
+	static HRESULT __fastcall DrawIndexedPrimitiveUp(int, int, IDirect3DDevice9 *, D3DPRIMITIVETYPE, UINT, UINT, UINT, const void *, D3DFORMAT,
+		const void *, UINT);
+};
